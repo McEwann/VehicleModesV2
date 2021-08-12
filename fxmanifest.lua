992a3069772ee745c47e9ed1c794c09bd9033d03
McEwan <88799568+McEwann@users.noreply.github.com>
2021-08-12 19:26:56 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'VehicleModes'
+description 'Custom Script to allow changing between handling modes of cars'
+author 'McEwan'
+version 'v 1.0'
+
+client_scripts {
+    'client/Main.lua',
+}
+
+server_scripts {
+    'server/Main.lua',
+}
